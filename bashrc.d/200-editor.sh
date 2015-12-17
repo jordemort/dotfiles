c8fd381feaa278f0269d66825aab611c5ca3afed
Jordan Webb <jordemort@github.com>
2015-12-17 09:27:50 -0800

    switch to trying to discover rmate on localhost first

--- bashrc.d/200-editor.sh
+++ bashrc.d/200-editor.sh
@@ -9,18 +9,18 @@ if [ -x "$subl" ] ; then
 else
 	[ -z "$RMATE_PORT" ] && export RMATE_PORT=62985
 
+	if [ -z "$RMATE_HOST" ] ; then
+		# try to discover rmate via localhost (presumably an SSH tunnel)
+		if bash -c "head -c0 < /dev/tcp/127.0.0.1/$RMATE_PORT" 2>/dev/null ; then
+			export RMATE_HOST=127.0.0.1
+		fi
+	fi
+
 	if [ -z "$RMATE_HOST" ] && [ ! -z "$SSH_CONNECTION" ] ; then
 		# try to discover rmate via direct connection
 		ssh_host=${SSH_CONNECTION%% *}
 		if bash -c "head -c0 < /dev/tcp/$ssh_host/$RMATE_PORT" 2>/dev/null ; then
 			export RMATE_HOST=$ssh_host
-		fi
-	fi
-
-	if [ -z "$RMATE_HOST" ] ; then
-		# try to discover rmate via localhost (presumably an SSH tunnel)
-		if bash -c "head -c0 < /dev/tcp/127.0.0.1/$RMATE_PORT" 2>/dev/null ; then
-			export RMATE_HOST=127.0.0.1
 		fi
 	fi
 
