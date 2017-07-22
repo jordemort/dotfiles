0edcf3e013df1f3632e315d8da2c315e004c4741
Jordan Webb <jordemort@github.com>
2017-07-22 15:33:55 -0500

    parasitize bash-it

---
+++ bashrc.d/700-bash-it.sh
@@ -0,0 +1,11 @@
+# Parasitizing bash-it for fun and profilt
+BASH_IT=$HOME/.homesick/repos/dotfiles/bash-it
+
+. $BASH_IT/lib/composure.bash
+cite _about _param _example _group _author _version
+
+. $BASH_IT/plugins/available/hub.plugin.bash
+for c in awscli gem hub pip pip3 vagrant virtualbox ; do
+  . $BASH_IT/completion/available/$c.completion.bash
+done
+. $BASH_IT/plugins/available/alias-completion.plugin.bash
