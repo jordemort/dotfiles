38a246b47ed09c5d4e81c219192a776a3c938fd0
Jordan Webb <jordemort@github.com>
2017-07-22 11:44:00 -0500

    update path

--- bashrc.d/000-path.sh
+++ bashrc.d/000-path.sh
@@ -1,24 +1,24 @@
-
 # build PATH
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 pre_path=(
+	"/usr/local/opt/coreutils/libexec/gnubin"
+	"/opt/X11/bin"
+	"/var/lib/gems/1.8/bin"
+	"/data/github/current/bin"
+	"/data/github/shell/bin"
+	"/data/ccql/current/bin"
+	"/data/orchestrator/current/bin"
+	"$HOME/Library/Python/*/bin"
+	"$HOME/.local/bin"
 	"$HOME/.homesick/repos/dotfiles/bin"
 	"$HOME/bin"
-	"$HOME/Library/Python/2.7/bin"
-	"$HOME/Library/Python/3.5/bin"
-    "$HOME/.local/bin"
-	"/opt/X11/bin"
-	"/opt/local/libexec/gnubin"
-	"/usr/local/opt/coreutils/libexec/gnubin"
-	"/opt/local/bin"
-	"/opt/local/sbin"
-	"/var/lib/gems/1.8/bin"
 )
 
 post_path=(
 	"/usr/local/games"
 	"/usr/games"
+	"/opt/puppetlabs/bin"
 	"/opt/dell/srvadmin/bin"
 )
 
