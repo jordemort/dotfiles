8fa12ef5abda7d34e66d85a5d9a456556c223ea2
Jordan Webb <jordemort@github.com>
2017-07-22 14:43:26 -0500

    try disabling commit signing if gpg agent isn't available

--- bashrc.d/800-gpg.sh
+++ bashrc.d/800-gpg.sh
@@ -1,5 +1,13 @@
 # load gpg agent environment
 if [ -e "$HOME/.gpg-agent-info" ] ; then
-    . $HOME/.gpg-agent-info
-    export GPG_AGENT_INFO
+  . $HOME/.gpg-agent-info
+  export GPG_AGENT_INFO
+  if [ ! -L $HOME/.gitconfig ] ; then
+    rm -f $HOME/.gitconfig
+    ln -s $HOME/.homesick/repos/dotfiles/home/.gitconfig $HOME/.gitconfig
+  fi
+elif [ -L $HOME/.gitconfig ] || [ $HOME/.homesick/repos/dotfiles/home/.gitconfig -nt $HOME/.gitconfig ] ; then
+  rm -f $HOME/.gitconfig
+  cp $HOME/.homesick/repos/dotfiles/home/.gitconfig $HOME/.gitconfig
+  git config --global commit.gpgsign false
 fi
