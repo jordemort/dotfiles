9c6f17d92cd246d4cad9e2ad2db09edcbb7ec46c
Jordan Webb <jordemort@github.com>
2017-07-25 11:22:38 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
-HOME/.homesick/repos/homeshick/bin/homeshick clone jordemort/dotfiles
+$HOME/.homesick/repos/homeshick/bin/homeshick clone jordemort/dotfiles
