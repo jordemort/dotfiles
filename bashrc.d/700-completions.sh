09018c117dc57af778ae0003bfd9b1c277a7fd13
Jordan Webb <jordemort@github.com>
2017-07-25 12:18:20 -0500

    maybe finally get completion compatibility right

--- bashrc.d/700-completions.sh
+++ bashrc.d/700-completions.sh
@@ -4,17 +4,31 @@ if [ "${BASH_VERSINFO[0]}" -gt "4" ] || [ "${BASH_VERSINFO[0]}" -eq "4" -a "${BA
 	BASH_COMPLETION_USER_DIR="$HOME/.homesick/repos/dotfiles/bash-completion/completions"
 fi
 
-if [ "${BASH_VERSINFO[0]}" -gt "4" ] || [ "${BASH_VERSINFO[0]}" -eq "4" -a "${BASH_VERSINFO[1]}" -ge "1" ] ; then
-	[ -z "$BASH_COMPLETION" ] && source "$HOME/.homesick/repos/dotfiles/bash-completion/bash_completion"
-elif [ -e "/etc/bash_completion" ] ; then
-	[ -z "$BASH_COMPLETION" ] && source "/etc/bash_completion"
+# old bash_completion versions set these vars, and if we try and load system
+# completions from one of those versions with our carpetbagged version we'll
+# get errors if we don't set this
+UNAME=$(uname -s)
+case ${UNAME} in
+    Linux|GNU|GNU/*) USERLAND=GNU ;;
+    *) USERLAND=${UNAME} ;;
+esac
+
+# if bash is new
+if [ -z "$BASH_COMPLETION" ] ; then
+	if [ "${BASH_VERSINFO[0]}" -gt "4" ] || [ "${BASH_VERSINFO[0]}" -eq "4" -a "${BASH_VERSINFO[1]}" -ge "1" ] ; then
+		source "$HOME/.homesick/repos/dotfiles/bash-completion/bash_completion"
+	elif [ -e "/etc/bash_completion" ] ; then
+		source "/etc/bash_completion"
+	fi
 fi
 
-# load extra completions
-[[ -x "$(which aws_completer)" ]] && complete -C "$(which aws_completer)" aws
+# always load these
 source "$HOME/.homesick/repos/dotfiles/bin/git-completion.bash"
 source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
-source "$HOME/.homesick/repos/dotfiles/open-bash-completion/open"
 source "$HOME/.homesick/repos/dotfiles/pip-bash-completion/pip"
-source "$HOME/.homesick/repos/dotfiles/vagrant-bash-completion/vagrant-bash-completion/etc/bash_completion.d/vagrant"
-source "$HOME/.homesick/repos/dotfiles/vboxmanage-bash-completion/VBoxManage"
+
+# load these as appropriate
+[ "$(type -P aws_completer)" ] && complete -C "$(which aws_completer)" aws
+[ "$UNAME" = "Darwin" ] && source "$HOME/.homesick/repos/dotfiles/open-bash-completion/open"
+[ "$(type -P vagrant)" ] && source "$HOME/.homesick/repos/dotfiles/vagrant-bash-completion/vagrant-bash-completion/etc/bash_completion.d/vagrant"
+[ "$(type -P vboxmanage)" ] && source "$HOME/.homesick/repos/dotfiles/vboxmanage-bash-completion/VBoxManage"
