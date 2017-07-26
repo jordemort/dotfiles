3b764ac546f5ddf0ab4fdb52088d3e8659d3ae9e
Jordan Webb <jordemort@github.com>
2017-07-26 00:52:35 -0500

    reinstante rmate (with UNIX socket support) as editor-of-choice

--- bashrc.d/200-editor.sh
+++ bashrc.d/200-editor.sh
@@ -2,15 +2,19 @@
 # no vi please
 export EDITOR="nano"
 
+alias atom=$EDITOR
+alias subl=$EDITOR
+
 atom=$(type -P atom)
 
 if [ -x "$atom" ] ; then
-	export EDITOR="atom -w"
+	export EDITOR="rmate -w"
 	alias nano="atom"
 	alias subl="atom"
-else
-	alias atom=$EDITOR
-	alias subl=$EDITOR
+elif [ -n "$RMATE_UNIX" ] ; then
+	export EDITOR="rmate -w"
+	alias nano="rmate"
+	alias subl="rmate"
 fi
 
 export VISUAL=$EDITOR
