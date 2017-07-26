ea9887f5f48f27601a33845150612a78874ff707
Jordan Webb <jordemort@github.com>
2017-07-26 01:29:19 -0500

    don't forward process-specific rmate socket

--- bashrc.d/100-sockets.sh
+++ bashrc.d/100-sockets.sh
@@ -2,6 +2,9 @@
 
 function cleanup_sockets() {
   [ -e "$RMATE_UNIX" ] && [ "$RMATE_UNIX" = "$HOME/.rmate.$$" ] && rm -f "$RMATE_UNIX"
+  other_rmate=$(ls --sort=t $HOME/.rmate.[0-9]* 2>/dev/null | head -n1)
+  [ -n "$other_rmate" ] && ln -sf "$other_rmate" "$HOME/.rmate.default"
+  
   [ -e "$HOME/.gnupg/S.gpg-agent.remote.$$" ] && rm -f "$HOME/.gnupg/S.gpg-agent.remote.$$"
   other_gpg_agent=$(ls --sort=t $HOME/.gnupg/S.gpg-agent.remote.* 2>/dev/null | head -n1)
   [ -n "$other_gpg_agent" ] && ln -sf "$other_gpg_agent" "$HOME/.gnupg/S.gpg-agent"
@@ -12,6 +15,7 @@ GPG_AGENT_UNIX=
 if [ -n "$SSH_CONNECTION" ] ; then
   if [ -e "$HOME/.rmate.in" ] ; then
     mv "$HOME/.rmate.in" "$HOME/.rmate.$$"
+    ln -sf "$HOME/.rmate.$$" "$HOME/.rmate.default"
     export RMATE_UNIX="$HOME/.rmate.$$"
   fi
   if [ -e "$HOME/.gnupg/S.gpg-agent.in" ] ; then
