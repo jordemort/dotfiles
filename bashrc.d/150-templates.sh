a5acf7c1f5a6acb52a45d653223abd79fc314388
Jordan Webb <jordemort@github.com>
2017-07-26 01:23:57 -0500

    make sure to remove old symlinks

--- bashrc.d/150-templates.sh
+++ bashrc.d/150-templates.sh
@@ -1,6 +1,6 @@
 #!bash
 
-[ -e "$HOME/.ssh/config" ] && [ ! -f "$HOME/.ssh/config" ] && rm -f "$HOME/.ssh/config"
+[ -e "$HOME/.ssh/config" ] && ([ ! -f "$HOME/.ssh/config" ] || [ -L "$HOME/.ssh/config" ]) && rm -f "$HOME/.ssh/config"
 if [ ! -e "$HOME/.ssh/config" ] || [ "$DOTFILES/home/.ssh/config.sempl" -nt "$HOME/.ssh/config" ] ; then
   sempl "$DOTFILES/home/.ssh/config.sempl" "$HOME/.ssh/config"
 fi
