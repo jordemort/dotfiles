c975326f8ca929f89a5dd71eec457d05709c1389
Jordan Webb <jordemort@github.com>
2018-01-09 12:37:14 -0600

    use short hostname in prompt colors

--- interactive.d/299-prompt-colors.sh
+++ interactive.d/299-prompt-colors.sh
@@ -4,7 +4,7 @@ COLORS_256=(3 4 5 6 10 12 13 14 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 
 
 # generate unique colors for user and host
 user_hash=$(echo $USER | cksum | cut -c2-5)
-host_hash=$(hostname -f | cksum | cut -c2-5)
+host_hash=$(hostname -s | cksum | cut -c2-5)
 
 if [[ $TERM =~ "256color" ]]; then
 	user_index=$(($user_hash % ${#COLORS_256[@]}))
