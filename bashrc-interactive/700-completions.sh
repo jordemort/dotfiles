9981615759a143d0ccb6fc7375a96051c0ad66ff
Jordan Webb <jordemort@github.com>
2018-01-25 10:47:45 -0600

    Stop toting around local copy of bash-completion

--- bashrc-interactive/700-completions.sh
+++ bashrc-interactive/700-completions.sh
@@ -1,34 +1,29 @@
 #!bash
-# load misc completions
-if [ "${BASH_VERSINFO[0]}" -gt "4" ] || [ "${BASH_VERSINFO[0]}" -eq "4" -a "${BASH_VERSINFO[1]}" -ge "1" ] ; then
-	BASH_COMPLETION_USER_DIR="$HOME/.homesick/repos/dotfiles/bash-completion/completions"
-fi
 
-# old bash_completion versions set these vars, and if we try and load system
-# completions from one of those versions with our carpetbagged version we'll
-# get errors if we don't set this
-UNAME=$(uname -s)
-case ${UNAME} in
-    Linux|GNU|GNU/*) USERLAND=GNU ;;
-    *) USERLAND=${UNAME} ;;
-esac
-
-# if bash is new
+# load bash_completion if it isn't already loaded for us
 if [ -z "$BASH_COMPLETION" ] ; then
-	if [ "${BASH_VERSINFO[0]}" -gt "4" ] || [ "${BASH_VERSINFO[0]}" -eq "4" -a "${BASH_VERSINFO[1]}" -ge "1" ] ; then
-		source "$HOME/.homesick/repos/dotfiles/bash-completion/bash_completion"
-	elif [ -e "/etc/bash_completion" ] ; then
-		source "/etc/bash_completion"
+	if [ -e /usr/local/etc/bash_completion ] ; then
+		# check for one installed by homebrew first
+		source /usr/local/etc/bash_completion
+	elif [ -e /etc/bash_completion ] ; then
+		# otherwise maybe it's here?
+		source /etc/bash_completion
 	fi
 fi
 
 # always load these
+# shellcheck source=../bin/git-completion.bash
 source "$HOME/.homesick/repos/dotfiles/bin/git-completion.bash"
+# shellcheck source=../../homeshick/completions/homeshick-completion.bash
 source "$HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash"
+# shellcheck source=../pip-bash-completion/pip
 source "$HOME/.homesick/repos/dotfiles/pip-bash-completion/pip"
 
 # load these as appropriate
 [ "$(type -P aws_completer)" ] && complete -C "$(which aws_completer)" aws
+# shellcheck source=../open-bash-completion/open
 [ "$UNAME" = "Darwin" ] && source "$HOME/.homesick/repos/dotfiles/open-bash-completion/open"
+# shellcheck source=../vagrant-bash-completion/vagrant-bash-completion/etc/bash_completion.d/vagrant
 [ "$(type -P vagrant)" ] && source "$HOME/.homesick/repos/dotfiles/vagrant-bash-completion/vagrant-bash-completion/etc/bash_completion.d/vagrant"
+# shellcheck source=../vboxmanage-bash-completion/VBoxManage
 [ "$(type -P vboxmanage)" ] && source "$HOME/.homesick/repos/dotfiles/vboxmanage-bash-completion/VBoxManage"
