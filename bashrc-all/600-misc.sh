f1b2d6a2880410691675528a87fa2a7062cc984a
Jordan Webb <jordemort@github.com>
2018-01-27 15:01:04 -0600

    Add initial secrets

--- bashrc-all/600-misc.sh
+++ bashrc-all/600-misc.sh
@@ -16,3 +16,6 @@ fi
 # set debian env based on git
 export DEBFULLNAME=$(git config --global user.name)
 export DEBEMAIL=$(git config --global user.email)
+
+# set secrets.sh key
+export SECRETS_GPG_KEY=158FACBF
