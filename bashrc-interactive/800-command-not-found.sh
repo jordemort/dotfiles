47531c0a8b7e1c6e72cd5028954b3696e65f917e
Jordan Webb <jordemort@github.com>
2018-01-28 00:09:19 -0600

    Freshen up command-not-found

--- bashrc-interactive/800-command-not-found.sh
+++ bashrc-interactive/800-command-not-found.sh
@@ -1,18 +1,23 @@
 #!bash
 
 # if the command-not-found package is installed, use it
-if [ -x /usr/lib/command-not-found -o -x /usr/share/command-not-found/command-not-found ]; then
-	function command_not_found_handle {
-		# check because c-n-f could've been removed in the meantime
-		if [ -x /usr/lib/command-not-found ]; then
-			/usr/lib/command-not-found -- "$1"
-			return $?
-		elif [ -x /usr/share/command-not-found/command-not-found ]; then
-			/usr/share/command-not-found/command-not-found -- "$1"
-			return $?
-		else
-			printf "%s: command not found\n" "$1" >&2
-			return 127
-		fi
-	}
-fi
+function detect_command_not_found {
+  if [ -x /usr/lib/command-not-found ] ; then
+    command_not_found=/usr/lib/command-not-found
+  elif [ -x /usr/share/command-not-found/command-not-found ]; then
+    command_not_found=/usr/lib/command-not-found
+  else
+    return
+  fi
+
+  function command_not_found_handle {
+    # check because c-n-f could've been removed in the meantime
+    if [ -x "$command_not_found" ]; then
+      "$command_not_found" -- "$1"
+    fi
+    printf "%s: command not found\n" "$1" >&2
+    return 127
+  }
+}
+
+detect_command_not_found
