26a382e775acf0f03c7b60974c8d875e5b59c01c
Jordan Webb <jordemort@github.com>
2020-03-16 17:06:03 -0500

    Move key import to setup.sh

--- bashrc-all/100-gpg-ssh-agent.sh
+++ bashrc-all/100-gpg-ssh-agent.sh
@@ -2,10 +2,3 @@
 
 gpgconf --launch gpg-agent
 export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
-
-if [ ! -e "$HOME/.gnupg/pubring.kbx" ] ; then
-  mkdir -p "$HOME/.gnupg"
-  chmod 700 "$HOME/.gnupg"
-  gpg --import < "$HOME/.homesick/repos/dotfiles/misc/key.pub"
-  echo "304E3D8D4A14C451DBA5582AC341950C47B6CE14:6:" | gpg --import-ownertrust
-fi
--- setup.sh
+++ setup.sh
@@ -17,3 +17,10 @@ if [ ! -d "$HOME/.homesick/repos/homeshick" ] ; then
 fi
 
 $HOME/.homesick/repos/homeshick/bin/homeshick link -f dotfiles
+
+if [ ! -e "$HOME/.gnupg/pubring.kbx" ] ; then
+  mkdir -p "$HOME/.gnupg"
+  chmod 700 "$HOME/.gnupg"
+  gpg --import < "$HOME/.homesick/repos/dotfiles/misc/key.pub"
+  echo "304E3D8D4A14C451DBA5582AC341950C47B6CE14:6:" | gpg --import-ownertrust
+fi
