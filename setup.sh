bbbe1218a7ff8b66579ca02f1e41ae5a328578dc
Jordan Webb <jordemort@github.com>
2020-03-16 16:45:26 -0500

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$(readlink -f "$0")" != "$HOME/.homesick/repos/dotfiles/setup.sh" ] ; then
-  mkdir -p "$HOME/.homesick/repos"
-  cp -a "$(dirname "$0")" "$HOME/.homesick/repos/dotfiles"
-  exec "$HOME/.homesick/repos/dotfiles/setup.sh"
+preferred_path="$HOME/.homesick/repos/dotfiles/setup.sh"
+preferred_dir="$(dirname "$preferred_path")"
+
+if [ "$(readlink -f "$0")" != "$preferred_path" ] ; then
+  if [ ! -e "$preferred_path" ] ; then
+    mkdir -p "$preferred_dir"
+    cp -a "$(dirname "$0")" "$preferred_dir"
+  fi
+  exec "$preferred_path"
 fi
 
 if [ ! -d "$HOME/.homesick/repos/homeshick" ] ; then
   git clone git://github.com/andsens/homeshick.git "$HOME/.homesick/repos/homeshick"
 fi
 
-$HOME/.homesick/repos/homeshick/bin/homeshick link -b dotfiles
+$HOME/.homesick/repos/homeshick/bin/homeshick link -f dotfiles
