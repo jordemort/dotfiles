499d4a949ba1422304399da398297424f284b1d7
Jordan Webb <jordemort@github.com>
2020-03-25 23:41:07 -0500

    Put remote VS Code into PATH

--- bashrc-all/200-editor.sh
+++ bashrc-all/200-editor.sh
@@ -2,14 +2,31 @@
 # no vi please
 EDITOR="nano"
 
+# Detect VS Code remote and inject it into path
+set -x
+if [ "$(uname -s)" = "Linux" ] ; then
+  ppid=$(ps -ho ppid "$$")
+  parent_exe=$(readlink -f "/proc/$ppid/exe")
+
+  if [[ $parent_exe = $HOME/.vscode-server/bin/* ]] ; then
+    vscode_bin="$(dirname "$parent_exe")/bin"
+    if [ -x "$vscode_bin/code" ] ; then
+      export PATH="$vscode_bin:$PATH"
+    fi
+  fi
+fi
+set +x
+
+# Look for VS Code
 code=$(type -P code)
 
 if [ -x "$code" ] ; then
+  # If we have it, alias everything over there
   EDITOR="code -n -w"
   alias nano=code
   alias subl=code
 else
-  alias atom=nano
+  # Otherwise, we want nano
   alias code=nano
   alias subl=nano
 fi
