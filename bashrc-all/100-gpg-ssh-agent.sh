5e74424e8dc656ca9e47da21b9532b045e66fa90
Jordan Webb <jordemort@github.com>
2020-04-24 10:40:07 -0500

    Don't relaunch gpg-agent if it's running

--- bashrc-all/100-gpg-ssh-agent.sh
+++ bashrc-all/100-gpg-ssh-agent.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-if [ "$(uname -o)" = "Darwin" ] ; then
-  gpgconf --launch gpg-agent
-  export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+if [ "$(uname -s)" = "Darwin" ] ; then
+  gpg-connect-agent /bye || gpgconf --launch gpg-agent
 fi
+
+export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
