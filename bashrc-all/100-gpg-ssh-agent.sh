b58e1ce563ec4d69fc7addbeb5f0c396bb2eb6dd
Jordan Webb <jordan@getseam.com>
2022-02-06 16:59:34 -0600

    Back to the Mac

--- bashrc-all/100-gpg-ssh-agent.sh
+++ bashrc-all/100-gpg-ssh-agent.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$HOSTNAME" = "betsy.webb.haus" ]; then
+if [ -n "$(type -P scutil)" ] && [ "$(scutil --get LocalHostName)" = "dorothy" ]; then
   gpg-connect-agent /bye || gpgconf --launch gpg-agent
   export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
