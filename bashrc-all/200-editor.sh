cc362b3780e18a2f4a81955c7a891ba00fa1652a
Jordan Webb <jordan@caketop.app>
2022-04-06 12:19:21 -0500

    Remove remote VSCode detection
    It no longer works, and VSCode Remote does the work of injecting a code binary
    into the path itself these days.

--- bashrc-all/200-editor.sh
+++ bashrc-all/200-editor.sh
@@ -2,25 +2,12 @@
 # no vi please
 EDITOR="nano"
 
-# Detect VS Code remote and inject it into path
-if [ "$(uname -s)" = "Linux" ] ; then
-  ppid=$(ps -ho ppid "$$")
-  parent_exe=$(readlink -f "/proc/${ppid//[[:space:]]/}/exe")
-
-  if [[ $parent_exe = $HOME/.vscode-server/bin/* ]] ; then
-    vscode_bin="$(dirname "$parent_exe")/bin"
-    if [ -x "$vscode_bin/code" ] ; then
-      path PATH prepend "$vscode_bin"
-    fi
-  fi
-fi
-
 # Look for VS Code
 code=$(type -P code)
 
 if [ -x "$code" ] ; then
   # If we have it, alias everything over there
-  EDITOR="code -n -w"
+  EDITOR="code -w"
   alias nano=code
   alias subl=code
 else
