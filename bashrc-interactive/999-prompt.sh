b963dd052ddb28f84625f5477195b1d59037e96f
Jordan Webb <jordan@caketop.app>
2022-04-06 09:37:07 -0500

    Use starship if available

--- bashrc-interactive/999-prompt.sh
+++ bashrc-interactive/999-prompt.sh
@@ -1,48 +1,65 @@
 #!/usr/bin/env bash
-# load git stuff
-# shellcheck disable=SC2034
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWUNTRACKEDFILES=1
-GIT_PS1_SHOWUPSTREAM=verbose
-GIT_PS1_SHOWCOLORHINTS=1
-
-# shellcheck source=generated/git-prompt.sh
-source "$HOME/.homesick/repos/dotfiles/generated/git-prompt.sh"
-
-PS1_BEFORE="\[${COLOR_USER}\]\u\[${COLOR_GRAY}\]@\[${COLOR_HOST}\]\h\[${COLOR_GRAY}\]:\[${COLOR_NONE}\]\w"
-PS1_AFTER="\[${COLOR_GRAY}\]\$\[${COLOR_NONE}\] "
 
 # figure out if we're setting window titles
-WINDOW_TITLE_MODE=
+__window_title_mode=
 case $TERM in
   screen*|rxvt|xterm*)
-    [ "$TERM_PROGRAM" = "Apple_Terminal" ] || WINDOW_TITLE_MODE="host"
+    [ "$TERM_PROGRAM" = "Apple_Terminal" ] || __window_title_mode="host"
   ;;
 esac
 
-function jordemort_prompt()
+__my_prompt_command()
 {
   local last_rc=$? last_status="" window_title=""
 
   # append to history file so we never lose anything
-  if [ -z "${mcfly:-}" ] ; then
+  if [ -z "${MCFLY_SESSION_ID:-}" ] ; then
     history -a
   fi
 
-  # add a red ! to the beginning if the last command failed
-  if [ "$last_rc" != "0" ] ; then
-    last_status="\[${COLOR_LIGHT_RED}\]${last_rc}! "
+  if [ "$__window_title_mode" = "host" ] ; then
+    echo -ne "\033]0;$USER@$HOSTNAME: $PWD\007"
   fi
 
-  if [ "$WINDOW_TITLE_MODE" = "host" ] ; then
-    window_title="\[\033]0;\u@\h: \w\007\]"
-  fi
+  if [ -z "${STARSHIP_SESSION_KEY:-}" ]; then
+    # add a red ! to the beginning if the last command failed
+    if [ "$last_rc" != "0" ] ; then
+      last_status="\[${COLOR_LIGHT_RED}\]${last_rc}! "
+    fi
 
-  # gitify the prompt
-  __git_ps1 "${last_status}${PS1_BEFORE}" "${PS1_AFTER}${window_title}"
+    # gitify the prompt
+    __git_ps1 "$last_rc ${last_status}${PS1_BEFORE}" "${PS1_AFTER}${window_title}"
+  fi
 
   return $last_rc
 }
 
-# update fancy prompt and append to history every command
-PROMPT_COMMAND="jordemort_prompt${PROMPT_COMMAND:+" ; $PROMPT_COMMAND"}"
+__configure_prompt() {
+  PROMPT_COMMAND=__my_prompt_command
+
+  if "$HOME/.homesick/repos/dotfiles/generated/it2check" 2> /dev/null; then
+    # shellcheck source=generated/iterm2_shell_integration.bash
+    source "$HOME/.homesick/repos/dotfiles/generated/iterm2_shell_integration.bash"
+  fi
+
+  if [ -z "${NO_STARSHIP:-}" ] && [ -x "$(type -P starship)" ]; then
+    eval "$(starship init bash)"
+  else
+    # shellcheck disable=SC2034
+    GIT_PS1_SHOWDIRTYSTATE=1
+    # shellcheck disable=SC2034
+    GIT_PS1_SHOWUNTRACKEDFILES=1
+    # shellcheck disable=SC2034
+    GIT_PS1_SHOWUPSTREAM=verbose
+    # shellcheck disable=SC2034
+    GIT_PS1_SHOWCOLORHINTS=1
+
+    # shellcheck source=generated/git-prompt.sh
+    source "$HOME/.homesick/repos/dotfiles/generated/git-prompt.sh"
+
+    PS1_BEFORE="\[${COLOR_USER}\]\u\[${COLOR_GRAY}\]@\[${COLOR_HOST}\]\h\[${COLOR_GRAY}\]:\[${COLOR_NONE}\]\w"
+    PS1_AFTER="\[${COLOR_GRAY}\]\$\[${COLOR_NONE}\] "
+  fi
+}
+
+__configure_prompt
