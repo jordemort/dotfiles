9497942742a296e59097da331904d49a91e36c26
Jordan Webb <jordan@jordemort.dev>
2025-01-21 15:51:58 -0600

    Switch to codium

--- bashrc-all/200-editor.sh
+++ bashrc-all/200-editor.sh
@@ -2,16 +2,26 @@
 # no vi please
 EDITOR="nano"
 
-# Look for VS Code
-code=$(type -P code)
+# Look for codium
+codium=$(type -P codium)
 
-if [ -x "$code" ]; then
+if [ -x "$codium" ] && [ -n "${SSH_CONNECTION:-}" ]; then
+	codium_dir=$(cd "$(dirname "$(which codium)")" && echo $(basename $(pwd)))
+	if [ "$codium_dir" != "remote-cli" ]; then
+		# Avoid using codium installed on remote system over SSH
+		codium=""
+	fi
+fi
+
+if [ -x "$codium" ]; then
 	# If we have it, alias everything over there
-	EDITOR="code -w"
-	alias nano=code
-	alias subl=code
+	EDITOR="codium -w"
+	alias code=codium
+	alias nano=codium
+	alias subl=codium
 else
 	# Otherwise, we want nano
+	alias codium=nano
 	alias code=nano
 	alias subl=nano
 fi
