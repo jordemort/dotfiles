b1632acaaa9653980988b065c04a7c19421a8c47
Jordan Webb <jordan@jordemort.dev>
2025-08-13 15:36:25 -0500

    Switch back to starship; fix mcfly PROMPT_COMMAND fuckery

--- bashrc-interactive/300-history.sh
+++ bashrc-interactive/300-history.sh
@@ -3,6 +3,10 @@
 if [ -x "$(type -P mcfly)" ]; then
 	export MCFLY_FUZZY=2
 	eval "$(mcfly init bash)"
+
+	# undo PROMPT_COMMAND array stuff mcfly does because it confuses everyone else
+	unset PROMPT_COMMAND
+	PROMPT_COMMAND="mcfly_prompt_command"
 fi
 
 if [ -z "${MCFLY_SESSION_ID:-}" ]; then
--- bashrc-interactive/999-prompt.sh
+++ bashrc-interactive/999-prompt.sh
@@ -1,74 +1,23 @@
 #!/usr/bin/env bash
 
-# figure out if we're setting window titles
-__window_title_mode=
-case $TERM in
-screen* | rxvt | xterm*)
-	[ "$TERM_PROGRAM" = "Apple_Terminal" ] || __window_title_mode="host"
-	;;
-esac
-
-__my_prompt_command() {
-	local last_rc=$? last_status=""
-
-	# append to history file so we never lose anything
-	if [ -z "${MCFLY_SESSION_ID:-}" ]; then
-		history -a
-	fi
-
-	if [ "$__window_title_mode" = "host" ]; then
-		echo -ne "\033]0;$USER@$HOSTNAME: $PWD\007"
-	fi
-
-	# add a red ! to the beginning if the last command failed
-	if [ "$last_rc" != "0" ]; then
-		last_status="\[${COLOR_LIGHT_RED}\]${last_rc}! "
-	fi
-
-	# gitify the prompt
-	__git_ps1 "${last_status}${PS1_BEFORE}" "${PS1_AFTER}"
-
-	return $last_rc
+set_window_title() {
+	echo -ne "\033]0;$USER@$HOSTNAME: $PWD\007"
+	echo -ne "\033]1337;CurrentDir=$PWD\007"
 }
 
-__configure_prompt() {
-	if [ -z "$PROMPT_COMMAND" ]; then
-		PROMPT_COMMAND="__my_prompt_command"
-	else
-		PROMPT_COMMAND="__my_prompt_command;${PROMPT_COMMAND#;}"
-	fi
+if [ -z "$PROMPT_COMMAND" ]; then
+	PROMPT_COMMAND="set_window_title"
+else
+	PROMPT_COMMAND="set_window_title;${PROMPT_COMMAND#;}"
+fi
 
-	# shellcheck source=submodules/iTerm2-shell-integration/shell_integration/bash
-	source "$HOME/.homesick/repos/dotfiles/submodules/iTerm2-shell-integration/shell_integration/bash"
+if [ -n "$(type -P starship)" ]; then
+    eval "$(starship init bash)"
+else
+    export PS1="$COLOR_USER\u$COLOR_NONE@$COLOR_HOST\h$COLOR_NONE:$COLOR_LIGHT_GREEN\w$COLOR_NONE\$ "
+fi
 
-	# shellcheck disable=SC2034
-	GIT_PS1_SHOWDIRTYSTATE=1
-	# shellcheck disable=SC2034
-	GIT_PS1_SHOWUNTRACKEDFILES=1
-	# shellcheck disable=SC2034
-	GIT_PS1_SHOWUPSTREAM=verbose
-	# shellcheck disable=SC2034
-	GIT_PS1_SHOWCOLORHINTS=1
+source "$HOME/.homesick/repos/dotfiles/submodules/iTerm2-shell-integration/shell_integration/bash"
 
-	# shellcheck source=generated/git-prompt.sh
-	source "$HOME/.homesick/repos/dotfiles/generated/git-prompt.sh"
-
-	PS1_BEFORE="\[\033[7m${COLOR_HOST}\]"
-
-	#if [ -n "${SSH_CONNECTION:-}" ] ; then
-	#  PS1_BEFORE="$PS1_BEFORE 🐚"
-	#fi
-
-	PS1_BEFORE="$PS1_BEFORE \h \[${COLOR_NONE}\033[7m\] \w \[${COLOR_NONE}\]"
-
-	if [ "$USER" != "jordan" ]; then
-		PS1_BEFORE="\[\033[7m${COLOR_USER}\] \u $PS1_BEFORE"
-	fi
-
-	PS1_AFTER="\n\[${COLOR_LIGHT_PURPLE}\]\$\[${COLOR_NONE}\] "
-}
-
-__configure_prompt
-
-# shellcheck disable=SC1090
-[[ "$TERM_PROGRAM" == "vscode" ]] && [ -n "$(type -P code)" ] && . "$(code --locate-shell-integration-path bash)"
+echo -ne "\033]1337;RemoteHost=$USER@$HOSTNAME\007"
+trap 'echo -ne "\033]0;$USER@$HOSTNAME: $PWD (${BASH_COMMAND})\007"' DEBUG
