82dbe8c81c0a5fa234491d63fa293f59db739ab0
Jordan Webb <jordan@jordemort.dev>
2025-08-13 15:46:50 -0500

    Alias pbcopy to it2copy when remote

--- bashrc-interactive/999-prompt.sh
+++ bashrc-interactive/999-prompt.sh
@@ -21,3 +21,7 @@ source "$HOME/.homesick/repos/dotfiles/submodules/iTerm2-shell-integration/shell
 
 echo -ne "\033]1337;RemoteHost=$USER@$HOSTNAME\007"
 trap 'echo -ne "\033]0;$USER@$HOSTNAME: $PWD (${BASH_COMMAND})\007"' DEBUG
+
+if [ -z "$(type -P pbcopy)" ]; then
+    alias pbcopy=it2copy
+fi
