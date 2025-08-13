2655f8d026c0c4114171f24a54bedf29ea0357de
Jordan Webb <jordan@jordemort.dev>
2025-08-13 16:08:20 -0500

    Steal update_terminal_cwd from Apple

--- bashrc-interactive/999-prompt.sh
+++ bashrc-interactive/999-prompt.sh
@@ -1,14 +1,41 @@
 #!/usr/bin/env bash
 
-set_window_title() {
+update_terminal_cwd() {
+    # Identify the directory using a "file:" scheme URL, including
+    # the host name to disambiguate local vs. remote paths.
+
+    # Percent-encode the pathname.
+    local url_path=''
+    {
+        # Use LC_CTYPE=C to process text byte-by-byte and
+        # LC_COLLATE=C to compare byte-for-byte. Ensure that
+        # LC_ALL and LANG are not set so they don't interfere.
+        local i ch hexch LC_CTYPE=C LC_COLLATE=C LC_ALL= LANG=
+        for ((i = 0; i < ${#PWD}; ++i)); do
+            ch="${PWD:i:1}"
+            if [[ "$ch" =~ [/._~A-Za-z0-9-] ]]; then
+                url_path+="$ch"
+            else
+                printf -v hexch "%02X" "'$ch"
+                # printf treats values greater than 127 as
+                # negative and pads with "FF", so truncate.
+                url_path+="%${hexch: -2:2}"
+            fi
+        done
+    }
+
 	echo -ne "\033]0;$USER@$HOSTNAME: $PWD\007"
 	echo -ne "\033]1337;CurrentDir=$PWD\007"
+    printf '\e]7;%s\a' "file://$HOSTNAME$url_path"
 }
 
+#set_window_title() {
+#}
+
 if [ -z "$PROMPT_COMMAND" ]; then
-	PROMPT_COMMAND="set_window_title"
+	PROMPT_COMMAND="update_terminal_cwd"
 else
-	PROMPT_COMMAND="set_window_title;${PROMPT_COMMAND#;}"
+	PROMPT_COMMAND="update_terminal_cwd;${PROMPT_COMMAND#;}"
 fi
 
 if [ -n "$(type -P starship)" ]; then
